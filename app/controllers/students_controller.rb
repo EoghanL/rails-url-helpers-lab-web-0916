90497779628bf9e8162493e32f1604c701f0088c
github username <github email address>
2016-10-21 13:08:18 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,19 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    set_student
+  end
+
+  def activate
+    @st = set_student
+    @st.active = !@st.active
+    @st.save
+    redirect_to student_path(@st)
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: 'jimbles', last_name: 'nutrombo')
+Student.create(first_name: 'mandi', last_name: 'ngo')
+Student.create(first_name: 'sofa', last_name: 'king')
+Student.create(first_name: 'mike', last_name: 'star')
